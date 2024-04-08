80400c61d1b4b5309d328d3c69eba56f96253c2a
Nikola-Majstorovic <mi19354@alas.matf.bg.ac.rs>
2024-04-08 10:55:48 +0200

    Changed readme file

--- src/main.cpp
+++ src/main.cpp
@@ -161,8 +161,10 @@ int main() {
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
+
 
     // build and compile shaders
     // -------------------------
